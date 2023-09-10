454750ab39c121a9744363081e0437cb6ce80630
Kampala <ezeanyakampala@gmail.com>
2023-09-10 02:03:38 +0100

    simple commt

--- app.js
+++ app.js
@@ -7,8 +7,27 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
+const api = process.env.PORT;
+
+//database
+const DB_URI = 'mongodb+srv://kampala:Kampala2810@cluster0.aixuzga.mongodb.net/?retryWrites=true&w=majority?ssl=true'
+
+ mongoose
+    .connect(DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ssl: true,
+        dbName: 'a-p-i_db'
+    })
+    .then(() => {
+        console.log('database connection successful');
+    })
+    .catch((err) => {
+        console.error('database connection error', err);
+    })
+    
 //server
-const PORT = process.env.port || 2810;
+const PORT = process.env.PORT || 2810;
 app.listen(PORT, () => {
-    console.log('server is running excently!')
-})
+    console.log('server is running excellently!')
+});
